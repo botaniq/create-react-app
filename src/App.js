f537bfc816e45e58f25a665d278f92639f140722
Vladyslav Rokhmaniuk <botaniqyes@gmail.com>
2023-05-21 16:18:48 +0300

    - test commit deploy

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Test
+            It is working
           </p>
           <LambdaDemo />
         </header>
